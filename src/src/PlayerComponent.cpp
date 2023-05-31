d6414d5ca905eba7515cbacd5a219537366b0f22
Edu-Git1 <eorellan@ucm.es>
2023-05-31 12:35:49 +0200

    Transform translate

--- src/src/PlayerComponent.cpp
+++ src/src/PlayerComponent.cpp
@@ -13,15 +13,15 @@ void PlayerController::start() {
 void PlayerController::update(float dt) {
     //std::cout << "PlayerController::update()\n";
     if (InputManager::Instance()->KeyPressed(SDL_SCANCODE_W)) {
-        _gameObject->getComponent<Transform>()->translate(0, -speed * dt);
+        gameObject->getComponent<Transform>()->translate(0, -speed * dt);
     }
     if (InputManager::Instance()->KeyPressed(SDL_SCANCODE_S)) {
-        _gameObject->getComponent<Transform>()->translate(0, speed * dt);
+        gameObject->getComponent<Transform>()->translate(0, speed * dt);
     }
     if (InputManager::Instance()->KeyPressed(SDL_SCANCODE_A)) {
-        _gameObject->getComponent<Transform>()->translate(-speed * dt, 0);
+        gameObject->getComponent<Transform>()->translate(-speed * dt, 0);
     }
     if (InputManager::Instance()->KeyPressed(SDL_SCANCODE_D)) {
-        _gameObject->getComponent<Transform>()->translate(speed * dt, 0);
+        gameObject->getComponent<Transform>()->translate(speed * dt, 0);
     }
 }
